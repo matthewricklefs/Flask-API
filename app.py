08d9b9bb72a46eafcdf39d5d266c1102a27c7afb
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-07 11:07:36 -0500

    add flask CORS and flask heroku

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+from flask_heroku import Heroku
+
 import os
 
 app = Flask(__name__)
+CORS(app)
+heroku = heroku(app)
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
